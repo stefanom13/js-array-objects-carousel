fee1bdf07e1211af85cbf6344f8968500d064772
stefanom13 <stefanomancuso13@gmail.com>
2022-02-21 16:29:17 +0100

    aggiunta forEach funzionante

--- js/app.js
+++ js/app.js
@@ -4,30 +4,30 @@
 // generate l ’html delle slide ciclando questo array e recuperando le informazioni necessarie della proprietà dei singoli oggetti.
 
 // inserimento array
-const slide =[
+const slide = [
     {
         title: 'Svezia',
-        description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
         image: 'img/01.jpg'
     },
     {
         title: 'Svizzera',
-        description:  'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
         image: 'img/02.jpg',
     },
     {
         title: 'Gran Bretagna',
-        description:  'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
         image: 'img/03.jpg',
     },
     {
         title: 'Germania',
-        description:  'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
         image: 'img/04.jpg',
     },
     {
         title: 'Paradise',
-        description:  'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
         image: 'img/05.jpg',
     },
 ]
@@ -62,15 +62,11 @@ const slideWrapper = document.querySelector('.slide-wrapper')
 const controlImage = document.querySelector('.secondaryImages')
 const buttSucc = document.querySelector('.up')
 const buttPrec = document.querySelector('.down')
-const itemImage = [...document.getElementsByClassName('item')];
-const itemControl = [...document.getElementsByClassName('image')];
 
 let currentIndex = 0
 
-itemImage[currentIndex].classList.add('active')
 
-
-slide.forEach( ({title,description,image}) => {
+slide.forEach(({ title, description, image }) => {
     const item = `
         <div class="item">
             <img src="${image}" alt="">
@@ -83,49 +79,26 @@ slide.forEach( ({title,description,image}) => {
     slideWrapper.innerHTML += item
 })
 
+const itemImage = [...document.getElementsByClassName('item')];
+itemImage[currentIndex].classList.add('active')
+
 
 // generare le slide con le immagini 
 // creare ciclio array di img
 
-// for (let i = 0; i < items.length; i++) {
-//     // creare elemeno html della slide 
-//     const item = `
-//         <div class="item">
-//             <img src="${items[i]}" alt="">
-//             <div class="description">
-//             <h5 class="titolo">${title[i]}</h5>
-//             <p class="paragrafo">${text[i]}</p>
-//             </div>
-//         </div>
-//         `
-//     slideWrapper.innerHTML += item
-// }
+slide.forEach(({ image }) => {
 
-
-slide.forEach( ({image}) => {
-   
     const boxImage = `
     <div class="controls ">
     <img class="image" src="${image}" alt="">
     </div>  
-    `;  
+    `;
     controlImage.innerHTML += boxImage;
-  }) 
+})
 
-  itemControl[currentIndex].classList.add('active')
+const itemControl = [...document.getElementsByClassName('image')];
+itemControl[currentIndex].classList.add('active')
 
-// creazione e controllo box img 
-// for (let i = 0; i < items.length; i++) {
-
-//     const boxImage = `
-//         <div class="controls">
-//         <img class="image" src="${items[i]}" alt="">
-//         </div>  
-//         `
-//     controlImage.innerHTML += boxImage
-// }
-
-//  itemControl[currentIndex].classList.add('active')
 
 
 // creare click down 
@@ -140,7 +113,7 @@ buttPrec.addEventListener('click', function () {
 
 
     } else {
-        currentIndex = items.length - 1
+        currentIndex = slide.length - 1
     }
     itemImage[currentIndex].classList.add('active')
     itemControl[currentIndex].classList.add('active')
@@ -153,7 +126,7 @@ buttSucc.addEventListener('click', function () {
     itemControl[currentIndex].classList.remove('active')
 
 
-    if (currentIndex < items.length - 1) {
+    if (currentIndex < slide.length - 1) {
         currentIndex++
         itemImage[currentIndex].classList.add('active')
         itemControl[currentIndex].classList.add('active')
